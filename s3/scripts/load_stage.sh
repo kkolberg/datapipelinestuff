f9c8aa3b2cdfb5764059b4e96440d0982614bd3d
Korey Kolberg <koreyk@slalom.com>
2017-02-08 13:13:09 -0500

    working

--- s3/scripts/load_stage.sh
+++ s3/scripts/load_stage.sh
@@ -19,17 +19,10 @@ aws s3 cp $s3SQL ./$object_$stage/start.sql
 aws s3 cp $s3finishedSQL ./$object_$stage/finished.sql
 aws s3 cp $s3errorSQL ./$object_$stage/error.sql
 
-sedSubs="-e s/\${redshiftschemaname}/$redshiftschemaname/ \
--e s/\${jobname}/$jobname/ \
--e s/\${columnlist}/$columnlist/ \
--e s/\${sfdcobject}/$sfdcobject/ \
--e s/\${s3jsonpathsfile}/$s3jsonpathsfile/ \
--e s/\${iamrole}/$iamrole/ \
--e s/\${stage}/$stage/"
 
-startSQL="$(cat "./$object_$stage/start.sql" | sed -n $sedSubs)"
-finishedSQL="$(cat "./$object_$stage/finished.sql" | sed -n $sedSubs)"
-errorSQL="$(cat "./$object_$stage/error.sql" | sed -n $sedSubs)"
+startSQL="$(cat "./$object_$stage/start.sql" | sed -e s^\$loggingschema^"$loggingschema"^ -e s^\$loggingtablename^"$loggingtablename"^ -e s^\${redshiftschemaname}^"$redshiftschemaname"^ -e s^\${jobname}^"$jobname"^ -e s^\${columnlist}^"$columnlist"^ -e s^\${sfdcobject}^"$sfdcobject"^ -e s^\${s3jsonpathsfile}^"$s3jsonpathsfile"^ -e s^\${iamrole}^"$iamrole"^ -e s^\${stage}^"$stage"^)"
+finishedSQL="$(cat "./$object_$stage/finished.sql" | sed -e s^\$loggingschema^"$loggingschema"^ -e s^\$loggingtablename^"$loggingtablename"^ -e s^\${redshiftschemaname}^"$redshiftschemaname"^ -e s^\${jobname}^"$jobname"^ -e s^\${columnlist}^"$columnlist"^ -e s^\${sfdcobject}^"$sfdcobject"^ -e s^\${s3jsonpathsfile}^"$s3jsonpathsfile"^ -e s^\${iamrole}^"$iamrole"^ -e s^\${stage}^"$stage"^)"
+errorSQL="$(cat "./$object_$stage/error.sql" | sed  -e s^\$loggingschema^"$loggingschema"^ -e s^\$loggingtablename^"$loggingtablename"^ -e s^\${redshiftschemaname}^"$redshiftschemaname"^ -e s^\${jobname}^"$jobname"^ -e s^\${columnlist}^"$columnlist"^ -e s^\${sfdcobject}^"$sfdcobject"^ -e s^\${s3jsonpathsfile}^"$s3jsonpathsfile"^ -e s^\${iamrole}^"$iamrole"^ -e s^\${stage}^"$stage"^)"
 
 export PGPASSWORD="$redshiftpassword"
 
